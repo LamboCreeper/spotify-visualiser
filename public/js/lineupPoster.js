5d9ff9efa325c4d08f0bc3f488169e32ac48356e
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-04-22 17:37:13 +0100

    Added the ability to customise the background and font colour of posters.

--- public/js/lineupPoster.js
+++ public/js/lineupPoster.js
@@ -1,6 +1,43 @@
 const poster = document.querySelector("#poster");
 const output = document.querySelector("#output");
 const downloadButton = document.querySelector("#poster-download");
+const selectPosterBackground = document.querySelector("#poster-background");
+const selectPosterText = document.querySelector("#poster-text");
+
+const colours = [
+    "white",
+    "black",
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "pink"
+];
+
+selectPosterBackground.addEventListener("change", ({ target }) => {
+    const { value } = target;
+
+    colours.forEach((colour) => {
+        if (poster.classList.contains(`poster-background-${colour}`)) {
+            poster.classList.remove(`poster-background-${colour}`);
+        }
+    });
+
+    poster.classList.add(`poster-background-${value}`);
+});
+
+selectPosterText.addEventListener("change", ({ target }) => {
+    const { value } = target;
+
+    colours.forEach((colour) => {
+        if (poster.classList.contains(`poster-text-${colour}`)) {
+            poster.classList.remove(`poster-text-${colour}`);
+        }
+    });
+
+    poster.classList.add(`poster-text-${value}`);
+});
 
 downloadButton.addEventListener("click", async () => {
     const canvas = await html2canvas(poster);
@@ -8,4 +45,5 @@ downloadButton.addEventListener("click", async () => {
 
     window.open(img);
 });
+
 (async () => output.append(await html2canvas(poster)))();
